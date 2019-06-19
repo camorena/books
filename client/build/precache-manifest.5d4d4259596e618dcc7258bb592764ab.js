4033a2e95c2c7711cb233d0b151c51f5ece253f4
camorena <camoren@gmail.com>
2019-06-18 23:26:51 -0500

    additional changes

---
+++ client/build/precache-manifest.5d4d4259596e618dcc7258bb592764ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d117b8b7bc3f6233ab3c",
+    "url": "/static/js/main.2d5adb3e.chunk.js"
+  },
+  {
+    "revision": "73a92ea81af2477f9057",
+    "url": "/static/js/2.44b3b530.chunk.js"
+  },
+  {
+    "revision": "d117b8b7bc3f6233ab3c",
+    "url": "/static/css/main.8f1e9d77.chunk.css"
+  },
+  {
+    "revision": "297ec77805e6c789570c20733b410a60",
+    "url": "/index.html"
+  }
+];
